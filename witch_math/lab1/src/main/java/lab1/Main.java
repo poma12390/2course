196cff9fde538ff6b8d5f8aa0851b34b0ccda0a3
poma12390 <kochnevr21@gmail.com>
2023-02-18 02:23:31 +0300

    end lab1

--- witch_math/lab1/src/main/java/lab1/Main.java
+++ witch_math/lab1/src/main/java/lab1/Main.java
@@ -1,6 +1,6 @@
 package lab1;
 
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 
 
@@ -10,7 +10,8 @@ public class Main {
 
     static Scanner sc;
     static int array_size;
-    static int number;
+    static float accuracy;
+
     private static void print_array(){
         for (int i = 0; i < array_size; i++){
             for (int j = 0; j < array_size+1; j++){
@@ -20,29 +21,83 @@ public class Main {
         }
     }
 
+    private static void inputFromKeyboard(){
+        System.out.print("Введите количество уравнений в матрице: ");
+        array_size = sc.nextInt();
+        input_array();
+        System.out.print("Введите точность(раздилитель запятая): ");
+        accuracy= sc.nextFloat();
+    }
+
+    private static void inputFromFile(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите путь к файлу: ");
+
+        try {
+            String file = br.readLine();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+
+            array_size= Integer.parseInt(reader.readLine());
+            accuracy=Float.parseFloat(reader.readLine());
+            array=new float[array_size][array_size+1];
+            int k=0;
+            while ((line = reader.readLine()) != null) {
+                    String[] line1 = line.trim().split(" ");
+                    for (int j=0; j<line1.length; j++) {
+                        array[k][j] = Float.parseFloat(line1[j]);
+                    }
+                    k+=1;
+
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("Файл не найден");
+        }
+        catch (IOException e) {
+            System.out.println("Проблемы с файлом");
+            System.exit(1);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Некорректное количество элементов в ряду ");
+            System.exit(1);
+        }
+    }
+//C:\Users\pomat\Documents\Itmo\2course\witch_math\lab1\src\main\resources\test.txt
     private static void input_array(){
+        System.out.println("Введите матрицу:");
         array = new float[array_size][array_size + 1];
         for (int i = 0; i < array_size; i++)
             for (int j = 0; j < array_size+1; j++)
                 array[i][j] = sc.nextInt();
     }
 
+
     public static void main(String[] args) {
         sc=new Scanner(System.in);
-        array_size = 3;
-        float accuracy=0.001f;
-        //input_array();
-        array= new float[][]{{5, -1, 3, 5}, {1, -4, 2, 20}, {2, -1, 5, 10}};
-        print_array();
-
-        Utils ut=new Utils(array_size);
-        ut.change_column(array);
-        ut.first_approx(array);
-        while(accuracy<ut.cur_accuracy){
-            ut.approximation(array, array_size);
-            System.out.println(ut.cur_accuracy);
+        System.out.print("Открыть из файла/Считать с клавиатуры (1/2):  ");
+        int type=sc.nextInt();
+        if (type==1){
+            inputFromFile();
+        } else if (type==2) {
+            inputFromKeyboard();
+        }else {
+            System.out.println("Другие варианты времмено недоступны");
         }
 
         print_array();
+        Utils ut=new Utils(array_size);
+        boolean test=(ut.check_diagonal(array));
+
+        if (test){
+            System.out.println("После проверки на диагональное преобладание");
+            print_array();
+            ut.refactor(array);
+            while(ut.cur_accuracy>accuracy){
+                ut.approximation(array, array_size);
+                ut.printResult();
+            }
+        }
+        else{
+            System.out.println("Невозможность достижения диагонального преобладания");
+        }
     }
 }
--- witch_math/lab1/src/main/java/lab1/Utils.java
+++ witch_math/lab1/src/main/java/lab1/Utils.java
@@ -1,22 +1,46 @@
 package lab1;
 
-import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Utils {
-    float[] cur_x;
+    private final float[] cur_x;
+
+    private final float[] xAccuracy;
+
+    private int n=0;
 
     float cur_accuracy=0;
 
     public Utils(int size) {
         cur_x= new float[size];
+        xAccuracy= new float[size];
         cur_accuracy=1;
     }
 
-    public void approximation(float[][] matrix, int array_size){
+    public void printResult(){
+        System.out.println(n + " Итерация");
+        System.out.print("Вектора неизвестных ");
+        for (int i=0; i<cur_x.length; i++) {
+            System.out.printf("x%d(%d)=%5.4f ", i+1, n, cur_x[i]);
+        }
+
+        if(n!=0) {System.out.println(); System.out.print("Вектора погрешностей ");}
+        for (int i=0; i<xAccuracy.length; i++) {
+            if(n==0) break;
+            System.out.printf("|x%d(%d)-x%d(%d)|=%5.4f ", i+1, n, i+1, n-1, xAccuracy[i]);
+        }
+        System.out.println();
+        System.out.printf("Точность %5.4f%n", cur_accuracy);
+
+    }
+
+    void approximation(float[][] matrix, int array_size){
+        n+=1;
         float k;
         cur_accuracy=0;
         for (int i=0; i<array_size; i++){
             k=cur_x[i];
+
             cur_x[i]=0;
             for (int j=0; j<array_size; j++){
                 if (i==j){
@@ -25,20 +49,36 @@ public class Utils {
                     cur_x[i]+=matrix[i][j]*cur_x[j]/matrix[i][array_size];
                 }
             }
-            cur_accuracy = Math.max(cur_accuracy, (Math.min(k, cur_x[i])/Math.max(k, cur_x[i]))-1);
+            xAccuracy[i]=Math.abs(k-cur_x[i]);
+            cur_accuracy = Math.max(cur_accuracy, ((Math.max(Math.abs(k), Math.abs(cur_x[i]))/Math.min(Math.abs(k), Math.abs(cur_x[i])))-1));
         }
 
-        System.out.println(Arrays.toString(cur_x));
+
     }
 
-    public void first_approx(float[][] matrix){
+    void first_approx(float[][] matrix){
         for (int i=0; i< Main.array_size; i++){
             cur_x[i]=matrix[i][i]/matrix[i][Main.array_size];
         }
-        System.out.println(Arrays.toString(cur_x));
+        printResult();
     }
 
-    public void change_column(float[][] matrix){
+    private void change_strings(float[][] matrix, int a, int b){
+        float k;
+        for (int i=0; i< Main.array_size; i++){
+            k=matrix[a][i];
+            matrix[a][i]=matrix[b][i];
+            matrix[b][i]=k;
+
+        }
+    }
+
+    void refactor(float[][] matrix){
+        change_column(matrix);
+        first_approx(matrix);
+    }
+
+    void change_column(float[][] matrix){
         float k;
         for (int i=0; i< Main.array_size; i++){
             k=-1*matrix[i][i];
@@ -47,6 +87,49 @@ public class Utils {
         }
 
     }
+    public boolean check_diagonal(float[][] matrix){
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        boolean isStrict=false;
+        for(int i=0; i<Main.array_size; i++){
+            for(int j=0; j<Main.array_size; j++){
+                int k=check_dominance(matrix, i, j);
+                if (k>=0){
+                    treeMap.put(j, i);
+                    if(k==1){
+                        isStrict=true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        if(treeMap.size()!=Main.array_size || !isStrict){
+            return false;
+        }
+        float[][] oldMatrix = new float[Main.array_size][Main.array_size+1];
+        for(int i=0; i<Main.array_size; i++){
+            if (Main.array_size + 1 >= 0) System.arraycopy(matrix[i], 0, oldMatrix[i], 0, Main.array_size + 1);
+        }
+        for (Integer newRow:treeMap.keySet()){
+            int oldRow = treeMap.get(newRow);
+            for(int j=0; j<Main.array_size+1; j++){
+                matrix[newRow][j]=oldMatrix[oldRow][j];
+            }
+        }
+
+        return true;
+    }
+
+    private int  check_dominance(float[][] matrix, int row, int column){
+        float element=Math.abs( matrix[row][column]);
+        float sum=-element;
+        for(int i=0; i<Main.array_size; i++){
+            sum+=Math.abs(matrix[row][i]);
+        }
+        return Float.compare(element, sum);
+    }
+
+
 
 
 }
