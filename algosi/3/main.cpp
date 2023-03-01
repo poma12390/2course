1b5884089373dea9c7cda4b31563d5f5d8c411ce
poma12390 <kochnevr21@gmail.com>
2023-03-01 22:40:01 +0300

    algo3 end

--- algosi/3/main.cpp
+++ algosi/3/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 #include <vector>
+#include <sstream>
+
 
 using namespace std;
 
@@ -9,61 +11,114 @@ vector<int> new_vector(int a){
     vector<int> vec={a};
     return vec;
 }
-bool isNumber(string& str)
-{
-    for (char const &c : str) {
-        if (isdigit(c) == 0)
-          return false;
+bool isNumber(const std::string& s) {
+    try {
+        stoi(s);
+    } catch (exception& e) {
+        return false;
     }
     return true;
 }
 
-map <string, vector<int>> push_all(map <string, vector<int>> m){
-    for (auto &i : m){
-        i.second.push_back(i.second.back());
-    }
-    return m;
+void push_all(vector <vector<string>> &history){
+    //cout << "{" << endl;
+    history.push_back(vector<string>{});
 }
 
-map <string, vector<int>> pop_all(map <string, vector<int>> m){
-    for (auto &i : m){
-        i.second.pop_back();
+void pop_all(unordered_map <string, vector<int>> &m, vector <vector<string>> &history){
+    vector<string> v = history.back();
+    history.pop_back();
+    for (string s: v){
+        //cout << " pop " << s;
+        m[s].pop_back();
     }
-    return m;
+    //cout << "}" << endl;
 }
+
+void var_to_var(unordered_map <string, vector<int>> &m, string &var_name, string &var_name1, unordered_map <string, vector<int>> :: iterator &it, vector <vector<string>> &history){
+    it = m.find(var_name);
+    if(it!=m.end()){
+        //cout<< var_name << " already= " << it->second[it->second.size()-1] << endl;
+    }
+    else{
+        //cout << "New var "<< var_name << endl;
+        m.insert({var_name, new_vector(0)});
+    }
+    it = m.find(var_name1);
+    if(it!=m.end()){
+       //cout<< var_name1 << " already= " << it->second[it->second.size()-1] << endl;
+    }
+    else{
+        //cout << "New var "<< var_name1 << endl;
+        m.insert({var_name1, new_vector(0)});
+    }
+    //cout<< var_name << " and " << var_name1 << " push" << endl;
+    history.back().push_back(var_name);
+
+    // for(auto a: m){
+    //    for(auto i: a.second){
+    //      cout << a.first << " = " << i;
+    //    }
+    //    cout << endl;
+    // }
+    cout<<m.find(var_name1)->second.back()  << endl;
+    m.find(var_name)->second.push_back(m.find(var_name1)->second.back());
+
+    
+}
+
+void int_to_var(unordered_map <string, vector<int>> &m, string var_name, int n, unordered_map <string, vector<int>> :: iterator &it, vector <vector<string>> &history){
+    it = m.find(var_name);
+    if(it==m.end()){
+        m.insert({var_name, new_vector(0)});
+        //cout << "New var(int_to_var) "<< var_name << endl;
+    }
+    //cout << var_name<<"=(int)"<<n<<endl;
+    history.back().push_back(var_name);
+    //cout<< var_name  << " push" << endl;
+    m.find(var_name)->second.push_back(n);
+}
+
+vector<string> split(string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+
 
 int main() {
     freopen("output.txt","w",stdout);
     freopen("input.txt","r",stdin);
     string s;
-    map <string, vector<int>> m;
-    map <string, vector<int>> :: iterator it;
-    // m.insert({"Poma", new_vector(147)});
-    // it = m.find("Poma");
-    // cout << "before " << it->second.size() << endl;
-    // m=push_all(m);
-    // it = m.find("Poma");
-    // cout << "middle " << it->second.size() << endl;
-    // m=pop_all(m);
-    // it = m.find("Poma");
-    // cout << "after " << it->second.size() << endl;
-
-    while (cin>>s)
-    { 
-        if(isNumber(s)){
-            cout<<"yes" << endl;
+    unordered_map <string, vector<int>> m;
+    unordered_map <string, vector<int>> :: iterator it;
+    vector<string> v;
+    vector <vector<string>> history;
+    history.push_back(vector<string>());
+    while (cin>>s){
+        if (s=="{"){
+            push_all(history);
+        }else if (s=="}")
+        {
+            pop_all(m, history);
         }
-        else{
-            cout << "no" << endl;
+        else{      
+            v=split(s, "=");
+            if(isNumber(v[1])){
+                int_to_var(m, v[0], stoi(v[1]), it, history);
+            }else{
+                var_to_var(m, v[0], v[1], it, history);
             }
-    }
-    
-    
-    // if(it!=m.end()){
-    //     cout << it->second[0];
-    // }
-    // else{
-    //     cout<<12;
-    // }
-
+        }
+        
+}
 }
