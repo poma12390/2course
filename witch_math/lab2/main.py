0f98e603cac9617a58edd1884399f3a35cc013ce
poma12390 <kochnevr21@gmail.com>
2023-03-11 01:01:17 +0300

    4

--- witch_math/lab2/main.py
+++ witch_math/lab2/main.py
@@ -1,5 +1,9 @@
 MAX_ITER=100
 
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
 def chord(f, a, b, e): #Находит
     x = (a + b)/2
     n=0
@@ -26,23 +30,41 @@ def chord(f, a, b, e): #Находит
 
 
 #F-Функция а-коефициенты при x_1 x_2, e-точность
+# def matrix_iter(F, a, e):
+#     X = [1, 1]
+#     phi = lambda X: [(-F[i](X) + a[i] * X[i])/a[i] for i in range(len(X))]#выражение x_1 x_2
+#     n=0
+#     while True:
+#         old_X = X
+#         X = phi(old_X)
+#         print("итерация", n)
+#         print(old_X[0], "x", n, "   ", X[0], "x", n + 1, end="  ")
+#         print(old_X[1], "y", n, "   ", X[1], "y", n + 1)
+#         n+=1
+#         if (n > MAX_ITER):
+#             print("Превышено число итераций")
+#             exit(1)
+#         return X
+#         if (max(abs(X[0] - old_X[0]), abs(X[1]-old_X[1])) <= e):
+#             pass
 def matrix_iter(F, a, e):
     X = [1, 1]
-    phi = lambda X: [(-F[i](X) + a[i] * X[i])/a[i] for i in range(len(X))]#выражение x_1 x_2
-    n=0
+    phi = lambda X: [(-F[i](X) + a[i] * X[i])/a[i] for i in range(len(X))]
+    n = 0
+    print(a)
+    print(phi([0,0]))
     while True:
+        if (n > MAX_ITER):
+            print("Превышено число итераций")
+            exit(1)
+        n+=1
         old_X = X
         X = phi(old_X)
         print("итерация", n)
         print(old_X[0], "x", n, "   ", X[0], "x", n + 1, end="  ")
         print(old_X[1], "y", n, "   ", X[1], "y", n + 1)
-        n+=1
-        if (n > MAX_ITER):
-            print("Превышено число итераций")
-            exit(1)
-        if (max(abs(X[0] - old_X[0]), abs(X[1]-old_X[1])) <= e):
+        if abs(X[0] - old_X[0]) <= e:
             return X
-
 
 def derivative(f, x, acc):
     return (f(x + acc) - f(x))/acc
@@ -68,7 +90,10 @@ def newton(f, df, a, b, e):
             return x
 
 
-
+def plot_func(f):
+    x = np.linspace(-5,5);
+    y = f(x);
+    plt.plot(x, y)
 
 
 def iteration_method(f, df, a, b, e):
@@ -97,5 +122,6 @@ def iteration_method(f, df, a, b, e):
 #print(chord(lambda x: x**3 + 2*x**2 - 5, 1, 2, 0.01))#Хорды
 #print(newton(lambda x: x**3 + 2*x**2 - 5,lambda x: 3*x**2 + 4*x, 1, 2, 0.01))
 print(matrix_iter([lambda X: 0.1 * X[0]**2 + X[0] + 0.2*X[1]**2 - 0.3,
-                   lambda X: 0.2 * X[1]**2 + X[1] + 0.1 * X[0] * X[1] - 0.7],
-      [1,1], 0.01))
+                    lambda X: 0.2 * X[1]**2 + X[1] + 0.1 * X[0] * X[1] - 0.7],
+       [1,1], 0.01))
+#plot_func(lambda x: x**3 + 2*x**2 - 5)
